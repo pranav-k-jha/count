35ba5a209dccedd7f1a29c9060ba839318902b36
pranav-k-jha <pranav.jha@mail.concordia.ca>
2024-05-07 19:50:27 -0400

    first commit

---
+++ App.jsx
@@ -0,0 +1,19 @@
+import Header from "./components/Header";
+import SearchBar from "./components/SearchBar";
+import SelectMenu from "./components/SelectMenu";
+
+import "./App.css";
+
+export default function App() {
+  return (
+    <>
+      <Header />
+      <main>
+        <div className="search-filter-container">
+          <SearchBar />
+          <SelectMenu />
+        </div>
+      </main>
+    </>
+  );
+}
